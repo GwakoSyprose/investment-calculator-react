457983a2c70fba062f60645506bab34a423154b4
syprose <syprosegwako@gmail.com>
2023-06-28 23:37:31 +0300

    using css modules

--- src/components/InvestmentForm/InvestmentForm.js
+++ src/components/InvestmentForm/InvestmentForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import classes from "./InvestmentForm.module.css";
 
 const initialUserInput = {
   "current-savings": 10000,
@@ -24,13 +25,13 @@ const InvestmentForm = (props) => {
     setUserInput((prevInput) => {
       return {
         ...prevInput,
-        [input]: value,
+        [input]: +value,
       };
     });
   };
   return (
-    <form onSubmit={submitHandler} className="form">
-      <div className="input-group">
+    <form onSubmit={submitHandler} className={classes.form}>
+      <div className={classes["input-group"]}>
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
           <input
@@ -54,7 +55,7 @@ const InvestmentForm = (props) => {
           />
         </p>
       </div>
-      <div className="input-group">
+      <div className={classes["input-group"]}>
         <p>
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
@@ -80,11 +81,15 @@ const InvestmentForm = (props) => {
           />
         </p>
       </div>
-      <p className="actions">
-        <button onClick={resetHandler} type="reset" className="buttonAlt">
+      <p className={classes.actions}>
+        <button
+          onClick={resetHandler}
+          type="reset"
+          className={classes.buttonAlt}
+        >
           Reset
         </button>
-        <button type="submit" className="button">
+        <button type="submit" className={classes.button}>
           Calculate
         </button>
       </p>
